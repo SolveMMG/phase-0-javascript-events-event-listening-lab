3b25f48644c04a7e4f5bf333af7953cffffb6050
Your Name <mikachupichu@gmail.com>
2023-09-27 20:44:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function addingEventListener() {
   const input = document.getElementById('button');
-
-input.addEventListener('click', function()
-   {alert('I was clicked!');
-  });
+  input.addEventListener('click', ()=> alert('I was clicked!'))
 }
